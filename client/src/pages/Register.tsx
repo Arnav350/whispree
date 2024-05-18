777077761efc82bd87e5c591556838eefbc954cb
Arnav Patel <patelarnavm+github@gmail.com>
2024-05-18 00:36:16 -0400

    started register page

---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Register() {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <button>Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;

66fa6ae6ce8cced5a91ef4d09fb27e90fa953eb4
Arnav Patel <patelarnavm+github@gmail.com>
2024-05-18 10:46:21 -0400

    created register screen styling

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import "../App.css";
 
 function Register() {
   return (
-    <div>
-      <form>
-        <input type="text" />
-        <input type="text" />
-        <button>Register</button>
-      </form>
+    <div className="register">
+      <div className="register-box">
+        <h1>Register</h1>
+        <form>
+          <div className="textbox">
+            <input type="text" placeholder="Username" required />
+          </div>
+          <div className="textbox">
+            <input type="password" placeholder="Password" required />
+          </div>
+          <input type="submit" className="btn" value="Register" />
+        </form>
+      </div>
     </div>
   );
 }
